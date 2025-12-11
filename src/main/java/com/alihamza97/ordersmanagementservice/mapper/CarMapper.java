3ea0feb1fb031a10d0085c6ea484b79eaa2db536
Ali Hamza <alihamza@Alis-MacBook-Pro.local>
2025-12-12 00:21:39 +0100

    added mapper

--- src/main/java/com/alihamza97/ordersmanagementservice/mapper/CarMapper.java
+++ src/main/java/com/alihamza97/ordersmanagementservice/mapper/CarMapper.java
@@ -3,30 +3,15 @@ package com.alihamza97.ordersmanagementservice.mapper;
 import com.alihamza97.ordersmanagementservice.dto.CarRequest;
 import com.alihamza97.ordersmanagementservice.dto.CarResponse;
 import com.alihamza97.ordersmanagementservice.model.Car;
+import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
-@Component
-public class CarMapper {
+@Mapper
+public interface CarMapper {
 
-    public Car toEntity(CarRequest req) {
-        Car car = new Car();
-        car.setId(UUID.randomUUID().toString());   // Cosmos id must be String
-        car.setCarModel(req.getCarModel());
-        car.setCarPrice(req.getCarPrice());
-        car.setCarName(req.getCarName());
+    Car toEntity(CarRequest req);
 
-        return car;
-    }
-
-    public CarResponse toResponse(Car car) {
-        CarResponse response = new CarResponse();
-        response.setId(car.getId());
-        response.setCarName(car.getCarName());
-        response.setCarPrice(car.getCarPrice());
-        response.setCarModel(car.getCarModel());
-        response.setCondition(car.getCondition());
-        return response;
-    }
+    CarResponse toResponse(Car car);
 }
