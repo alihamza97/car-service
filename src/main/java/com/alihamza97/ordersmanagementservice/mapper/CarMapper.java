fe2b80a4828c82d413e1ada2d6b675a504b67c66
Ali Hamza <alihamza@Alis-MacBook-Pro.local>
2025-12-12 03:33:55 +0100

    cleanup

--- src/main/java/com/alihamza97/ordersmanagementservice/mapper/CarMapper.java
+++ src/main/java/com/alihamza97/ordersmanagementservice/mapper/CarMapper.java
@@ -3,7 +3,6 @@ package com.alihamza97.ordersmanagementservice.mapper;
 import com.alihamza97.ordersmanagementservice.dto.CarRequest;
 import com.alihamza97.ordersmanagementservice.dto.CarResponse;
 import com.alihamza97.ordersmanagementservice.model.Car;
-import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
